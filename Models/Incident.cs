68a287b4fed240787606bfde17d48c2edbc5e272
Kaya <love4stephanie@protonmail.com>
2022-02-24 02:04:09 -0500

    some form edited

--- Models/Incident.cs
+++ Models/Incident.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.ComponentModel;
-
 
 namespace assignment1.Models
 {
@@ -13,9 +10,11 @@ namespace assignment1.Models
         public long IncidentId { get; set; }
 
         [Required(ErrorMessage = "Customer is required")]
-        public Customer Customer { get; set;}
+        public long CustomerId { get; set; }
+        public Customer Customer { get; set; }
 
         [Required(ErrorMessage = "Product is required")]
+        public string ProductCode { get; set; }
         public Product Product { get; set; }
 
         [Required(ErrorMessage = "Title is required")]
@@ -24,9 +23,13 @@ namespace assignment1.Models
         [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
+#nullable enable
+        public long? TechnicianId { get; set; }
         public Technician? Technician { get; set; }
+#nullable restore
 
         public DateTime? DateOpened { get; set; }
+
         [Required(ErrorMessage = "Date Closed is required")]
         public DateTime DateClosed { get; set; }
     }
