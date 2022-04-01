cade9672a03d1bd8f8311e6bff3c4a80e2bbbf68
multimokia <multimokia3@gmail.com>
2022-04-01 12:00:35 -0400

    some better defaulting

--- Models/Incident.cs
+++ Models/Incident.cs
@@ -9,18 +9,18 @@ namespace assignment1.Models
         [Key]
         public long IncidentId { get; set; }
 
-        [Required(ErrorMessage = "Customer is required")]
+        [Required(ErrorMessage = "Customer is required.")]
         public long CustomerId { get; set; }
         public Customer Customer { get; set; }
 
-        [Required(ErrorMessage = "Product is required")]
+        [Required(ErrorMessage = "Product is required.")]
         public string ProductId { get; set; }
         public Product Product { get; set; }
 
-        [Required(ErrorMessage = "Title is required")]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Description is required")]
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
 
         #nullable enable
@@ -29,11 +29,11 @@ namespace assignment1.Models
         #nullable restore
 
         [Display(Name="Date Opened")]
-        public DateTime? DateOpened { get; set; }
+        [Required(ErrorMessage = "Open date is required.")]
+        public DateTime DateOpened { get; set; } = DateTime.Now;
 
         [Display(Name="Date Closed")]
-        [Required(ErrorMessage = "Date Closed is required")]
-        public DateTime DateClosed { get; set; }
+        public DateTime? DateClosed { get; set; }
 
         //Filterable statuses
         [NotMapped]
--- Models/Person.cs
+++ Models/Person.cs
@@ -13,26 +13,26 @@ namespace assignment1.Models
         /// <summary>
         /// Full name of the person
         /// /// </summary>
-        [Required(ErrorMessage="Name is required")]
+        [Required(ErrorMessage="Name is required.")]
         [Display]
         public string Name { get; set; }
 
         /// <summary>
         /// Street address of the person, (no country/city/postal code)
         /// </summary>
-        [Required(ErrorMessage="Address is required")]
+        [Required(ErrorMessage="Address is required.")]
         public string StreetAddress { get; set; }
 
         /// <summary>
         /// Person's city
         /// </summary>
-        [Required(ErrorMessage="City is required")]
+        [Required(ErrorMessage="City is required.")]
         public string City { get; set; }
 
         /// <summary>
         /// Person's postal code
         /// </summary>
-        [Required(ErrorMessage="Postal code is required")]
+        [Required(ErrorMessage="Postal code is required.")]
         public string PostalCode { get; set; }
 
         /// <summary>
@@ -45,14 +45,14 @@ namespace assignment1.Models
         /// <summary>
         /// Person's email address (optional)
         /// </summary>
-        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is not valid")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is not valid.")]
         public string? EmailAddress { get; set; }
 
         /// <summary>
         /// Person's phone number (optional)
         /// </summary>
         [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^\(\d{3}\)-\d{3}-\d{4}$", ErrorMessage = "Phone number must be in format (xxx)-xxx-xxx")]
+        [RegularExpression(@"^\(\d{3}\)-\d{3}-\d{4}$", ErrorMessage = "Phone number must be in format '(xxx)-xxx-xxx'.")]
         public string? PhoneNumber { get; set; }
 
         #nullable restore
