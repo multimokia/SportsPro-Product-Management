0168f4ed415e5bd4dec2dd4aba83dfcdc7ba0dd3
eric <erickim334@gmail.com>
2022-04-02 22:23:31 -0400

    just some updates

--- Models/Registration.cs
+++ Models/Registration.cs
@@ -1,8 +1,15 @@
-﻿using System;
-
+﻿using System.ComponentModel.DataAnnotations;
 namespace assignment1.Models
 {
     public class Registration
     {
+        [Key]
+        public int RegistrationId { get; set; }
+
+        public long CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
---
+++ ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using assignment1.Models;
+
+namespace assignment1.ViewModels
+{
+    public class RegistrationViewModel : PageModel
+    {
+        public enum FilterOption
+        {
+            All,
+            Open,
+            Closed,
+            Unassigned
+        }
+
+        public FilterOption Filter { get; set; }
+    }
+}
